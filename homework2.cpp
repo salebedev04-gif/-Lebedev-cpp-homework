4479cb29d73333f050ad6c157ef55e16bc806a64
salebedev04-gif <s.a.lebedev.04@gmail.com>
2025-10-08 18:36:27 +0500

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    int z;
+    cout << "введите первое число";
+    cin >> x;
+    cout << "введите второе число";
+    cin >> y;
+    cout << "введите третье число";
+    cin >> z;
+    double xyz = (x + y + z)/3.0;
+    cout << "среднее арефмитическое =" << xyz;
+    
+}
+
+
+
+
+
+
